bd6d3e8ba9add47d7642ee450d7e573552bb36a2
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-11-07 21:28:22 -0500

    add database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/", (c) => c.text("Hono!"));
+
+const server = Bun.serve({
+  fetch: app.fetch,
+  port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+});
+
+console.log(`lmk server running at ${server.url}`);
