cd23cff2cc93c3569a07a8d124aba9c3ebaea71a
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-11-08 01:09:42 -0500

    aaaa

--- expo/app/(tabs)/home.tsx
+++ expo/app/(tabs)/home.tsx
@@ -2,7 +2,7 @@ import { signOutOptions } from "@/lib/auth-client";
 import { trpc } from "@/lib/trpc";
 import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
 import { Redirect } from "expo-router";
-import { Button, Text } from "react-native";
+import { Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
@@ -13,10 +13,20 @@ export default function HomeScreen() {
   if (!user.data) return <Redirect href="/" />;
 
   return (
-    <SafeAreaView className="text-white">
-      <Text>Hi {user.data?.name}</Text>
+    <SafeAreaView className="text-white p-3">
+      <View className="flex items-center justify-between gap-4 flex-row">
+        <Text className="text-white text-2xl font-medium">
+          Good morning, {user.data?.name.split(" ")[0]}
+        </Text>
 
-      <Button title="Logout" onPress={() => signOut.mutate()} />
+        <Image
+          source={{
+            uri: user.data.image!,
+          }}
+          className="size-12 rounded-full"
+        />
+        {/* <Button title="Logout" onPress={() => signOut.mutate()} /> */}
+      </View>
     </SafeAreaView>
   );
 }
