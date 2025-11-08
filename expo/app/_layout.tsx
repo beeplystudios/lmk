4fc46fd7c09ce5e1f4784d37104a3645e4ec1f7e
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-11-08 01:19:58 -0500

    push more

--- expo/app/_layout.tsx
+++ expo/app/_layout.tsx
@@ -8,10 +8,13 @@ import { useColorScheme } from "@/hooks/use-color-scheme";
 import "@/global.css";
 import { queryClient } from "@/lib/trpc";
 import { QueryClientProvider } from "@tanstack/react-query";
+import * as SystemUI from "expo-system-ui";
 
 // export const unstable_settings = {
 //   anchor: "(tabs)",
 // };
+
+SystemUI.setBackgroundColorAsync("black");
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -19,11 +22,17 @@ export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       {/* <ThemeProvider value={DarkTheme}> */}
-      <Stack>
+      <Stack
+        screenOptions={{
+          contentStyle: {
+            backgroundColor: "#18181b",
+          },
+        }}
+      >
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       {/* </ThemeProvider> */}
     </QueryClientProvider>
   );
