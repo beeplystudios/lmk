0333b903ecf5b7a6a6606b4a342f08447eafe96a
Nirjhor Nath <nirjhor.nath@gmail.com>
2025-11-07 22:32:19 -0500

    trpc!!

--- expo/app/(tabs)/index.tsx
+++ expo/app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
+import { trpc } from "@/lib/trpc";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
+  const { data } = useSuspenseQuery(
+    trpc.greet.queryOptions({
+      name: "Nirjhor",
+    })
+  );
+
   return (
     <SafeAreaView>
-      <Text>aAAaa</Text>
+      <Text>{data}</Text>
     </SafeAreaView>
   );
 }
