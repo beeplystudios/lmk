6646330431df0bf314656047ed6b37a53cac5485
SantiagoVira <santiagovira06@gmail.com>
2025-11-09 07:38:53 -0500

    explore links

--- expo/app/(tabs)/explore.tsx
+++ expo/app/(tabs)/explore.tsx
@@ -1,8 +1,16 @@
 import { trpc } from "@/lib/trpc";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Redirect } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
 import { useCallback } from "react";
-import { Image, RefreshControl, ScrollView, Text, View } from "react-native";
+import {
+  Image,
+  Pressable,
+  RefreshControl,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 const headlines = new Array(10).fill(0).map((_, idx) => ({
   id: idx + "",
@@ -51,35 +59,45 @@ export default function ExploreScreen() {
 
         <View className="pb-24 flex flex-col gap-4">
           {data.data.map((lmk) => (
-            <View key={lmk.id} className="bg-zinc-800 rounded-xl relative">
-              <View className="flex flex-col gap-4">
-                {lmk.image && (
-                  <Image
-                    source={{
-                      uri: lmk.image,
-                      width: 500,
-                      height: 400,
-                    }}
-                    className="w-full object-cover rounded-xl"
-                  />
-                )}
-                <View
-                  className={
-                    !lmk.image
-                      ? "p-4"
-                      : "absolute bg-black/80 bottom-0 p-4 w-full rounded-b-xl"
-                  }
-                >
-                  <Text className="text-white text-lg font-medium font-serif">
-                    {lmk.title}
-                  </Text>
-                  <Text className="text-zinc-400">{lmk.description}</Text>
-                  <Text className="text-stone-400 mt-2">
-                    From the {lmk.source}
-                  </Text>
+            <Pressable
+              key={lmk.id}
+              onPress={() => {
+                const link = lmk?.link ?? "";
+                if (link) {
+                  WebBrowser.openBrowserAsync(link);
+                }
+              }}
+            >
+              <View className="bg-zinc-800 rounded-xl relative">
+                <View className="flex flex-col gap-4">
+                  {lmk.image && (
+                    <Image
+                      source={{
+                        uri: lmk.image,
+                        width: 500,
+                        height: 400,
+                      }}
+                      className="w-full object-cover rounded-xl"
+                    />
+                  )}
+                  <View
+                    className={
+                      !lmk.image
+                        ? "p-4"
+                        : "absolute bg-black/80 bottom-0 p-4 w-full rounded-b-xl"
+                    }
+                  >
+                    <Text className="text-white text-lg font-medium font-serif">
+                      {lmk.title}
+                    </Text>
+                    <Text className="text-zinc-400">{lmk.description}</Text>
+                    <Text className="text-stone-400 mt-2">
+                      From the {lmk.source}
+                    </Text>
+                  </View>
                 </View>
               </View>
-            </View>
+            </Pressable>
           ))}
           {data.data.length === 0 && (
             <Text className="text-zinc-400 italic">
